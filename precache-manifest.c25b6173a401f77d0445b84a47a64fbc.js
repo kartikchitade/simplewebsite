dfb4a47ea5695c751e71741fdef34ddc88820f33
kartikchitade <kartikchitade@users.noreply.github.com>
2020-08-09 10:55:40 +0530

    Updates

---
+++ precache-manifest.c25b6173a401f77d0445b84a47a64fbc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a676147b9deaf1fccfc5fb88bdbb2d",
+    "url": "/simplewebsite/index.html"
+  },
+  {
+    "revision": "13227c2bccbe908a273f",
+    "url": "/simplewebsite/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8a51b516ccccc94430fb",
+    "url": "/simplewebsite/static/css/main.7808aa17.chunk.css"
+  },
+  {
+    "revision": "13227c2bccbe908a273f",
+    "url": "/simplewebsite/static/js/2.e3f54ef9.chunk.js"
+  },
+  {
+    "revision": "48569a0863e86e8b00cf55db963d6025",
+    "url": "/simplewebsite/static/js/2.e3f54ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a51b516ccccc94430fb",
+    "url": "/simplewebsite/static/js/main.7881ee87.chunk.js"
+  },
+  {
+    "revision": "c04105fdd6c540357d21",
+    "url": "/simplewebsite/static/js/runtime-main.3c917191.js"
+  },
+  {
+    "revision": "3de3755d07928f6d3edddf08455a569e",
+    "url": "/simplewebsite/static/media/web1.3de3755d.jpg"
+  },
+  {
+    "revision": "e89cb54ab57230c285167655397ff33e",
+    "url": "/simplewebsite/static/media/web2.e89cb54a.jpg"
+  },
+  {
+    "revision": "f297d40ab78eb61397576d5873d6159c",
+    "url": "/simplewebsite/static/media/web3.f297d40a.jpg"
+  },
+  {
+    "revision": "29edb9da51ff594c20a1ef905f0c99c3",
+    "url": "/simplewebsite/static/media/web4.29edb9da.jpg"
+  },
+  {
+    "revision": "104e08ae59e234086e6c852b16a57715",
+    "url": "/simplewebsite/static/media/web5.104e08ae.jpg"
+  },
+  {
+    "revision": "baebe47302e502f6172d21ea9d2da589",
+    "url": "/simplewebsite/static/media/web6.baebe473.jpg"
+  },
+  {
+    "revision": "bd935430df807d8d2b49bd3406e5a44b",
+    "url": "/simplewebsite/static/media/web7.bd935430.png"
+  },
+  {
+    "revision": "cf98eb11ee1e60531d31aeb26c4d04f1",
+    "url": "/simplewebsite/static/media/web8.cf98eb11.png"
+  }
+]);
